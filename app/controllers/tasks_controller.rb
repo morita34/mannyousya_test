f597f0f6c278732b7be63146d470557651642275
morita <morita97job@gmail.com>
2023-07-20 02:52:46 +0900

    step1-re

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,52 +1,49 @@
 class TasksController < ApplicationController
-  def show
-    @task = Task.find(params[:id])
+  before_action :set_task, only: %i[ show edit update destroy ]
+
+  def index
+    @tasks = Task.all
   end
 
   def new
     @task = Task.new
   end
 
-  def index
-    @tasks = Task.all
-  end
-
   def create
     @task = Task.new(task_params)
-
     if @task.save
-      flash[:notice] = 'Task was successfully created.'
-      redirect_to tas_path
+      redirect_to tasks_path, notice: 'Task was successfully created'
     else
       render :new
     end
   end
 
+  def show
+  end
+
   def edit
-    @task = Task.find(params[:id])
   end
 
   def update
-    @task = Task.find(params[:id])
-
     if @task.update(task_params)
-      flash[:notice] = 'Task was successfully updated.'
-      redirect_to task_path(@task)
+      redirect_to tasks_path, notice: 'Task was successfully updated.'
     else
       render :edit
     end
   end
 
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
-    flash[:notice] = 'Task was successfully destroyed.'
-    redirect_to tasks_url
+    redirect_to tasks_path, notice: 'Task was successfully destroyed.'
   end
 
   private
 
-  def task_params
-    params.require(:task).permit(:title, :content)
-  end
+    def set_task
+      @task = Task.find(params[:id])
+    end
+
+    def task_params
+      params.require(:task).permit(:title, :content)
+    end
 end
--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -3,15 +3,12 @@ require 'rails_helper'
 RSpec.describe 'タスク管理機能', type: :system do
   describe '登録機能' do
     context 'タスクを登録した場合' do
-      before do
-        visit new_task_path
-        fill_in 'title', with: '書類作成'
-        fill_in 'content', with: '企画書を作成する。'
-        click_button 'Create Task'
-      end
-
       it '登録したタスクが表示される' do
-        expect(page).to have_content('書類作成')
+        visit new_task_path
+        fill_in 'Title', with: '書類作成'
+        fill_in 'Content', with: '企画書を作成する'
+        click_button 'Create Task'
+        expect(page).to have_content '書類作成'
       end
     end
   end
@@ -23,8 +20,6 @@ RSpec.describe 'タスク管理機能', type: :system do
         FactoryBot.create(:task)
         # タスク一覧画面に遷移
         visit tasks_path
-        # ページ遷移直後にデバッグ
-        binding.irb
         # visit（遷移）したpage（この場合、タスク一覧画面）に"書類作成"という文字列が、have_content（含まれていること）をexpect（確認・期待）する
         expect(page).to have_content '書類作成'
         # expectの結果が「真」であれば成功、「偽」であれば失敗としてテスト結果が出力される
@@ -33,14 +28,10 @@ RSpec.describe 'タスク管理機能', type: :system do
   end
 
   describe '詳細表示機能' do
-     context '任意のタスク詳細画面に遷移した場合' do
-      before do
-        FactoryBot.create(:task)
-        visit tasks_path
-        click_on 'Show'
-      end
-
+    context '任意のタスク詳細画面に遷移した場合' do
       it 'そのタスクの内容が表示される' do
+        @task = FactoryBot.create(:task)
+        visit tasks_path(@task)
         expect(page).to have_content '書類作成'
       end
     end
