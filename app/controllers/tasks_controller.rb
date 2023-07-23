06f9978932b919ee37f3edec13f8b3bf3b67076f
morita <morita97job@gmail.com>
2023-07-24 03:36:05 +0900

    step1

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -15,7 +15,7 @@ class TasksController < ApplicationController
   def create
     @task = Task.new(task_params)
     if @task.save
-      redirect_to tasks_path, notice: 'Task was successfully created'
+      redirect_to tasks_path, notice: 'Task was successfully created.'
     else
       render :new
     end
@@ -26,7 +26,7 @@ class TasksController < ApplicationController
 
   def update
     if @task.update(task_params)
-      redirect_to tasks_path, notice: 'Task was successfully updated.'
+      redirect_to task_path(@task), notice: 'Task was successfully updated.'
     else
       render :edit
     end
